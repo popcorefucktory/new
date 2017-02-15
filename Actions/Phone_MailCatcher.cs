156c7445d7ee6a2c265f068cfa02fd92479125a5
popcorefucktory <madforcarrie@gmail.com>
2017-02-15 13:55:45 +0300

    Add files via upload

---
+++ Actions/Phone_MailCatcher.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace TestProjectNew.Actions
+{
+    public static class Phone_MailCatcherActions
+    {
+        public static void OpenMailcatcher(RemoteWebDriver session)
+        {
+
+            ChromeDriver openMailCatcher = new ChromeDriver();
+            openMailCatcher.Url = "http://controller.kivyanskaya.notkube.v.netstream.ru:1080/";
+            openMailCatcher.Navigate();
+
+            openMailCatcher.FindElementByXPath("//*[@id='messages']/table/tbody/tr/td[2]").Click();
+            //openMailCatcher.FindElementByXPath("//*[@id='message']/header/nav/ul/li[4]/a/span").Click();
+            //var key = openMailCatcher.FindElementById("/html/body/table/tbody/tr/td[2]").Text;
+            //Console.WriteLine(key);
+            var iframe = openMailCatcher.SwitchTo().Frame(openMailCatcher.FindElementByXPath("//*[@id='message']/iframe"));
+            var key = openMailCatcher.FindElementByXPath("/html/body").Text;
+            Console.WriteLine(key);
+            _session.FindElementByClassName("TextBlock").SendKeys(key);
+            _session.FindElementByName("Войти").SendKeys(Keys.Enter);
+            _session.FindElementByClassName("Button").SendKeys(Keys.Enter);
+            Thread.Sleep(3000);
+
+            var mainTheme = _session.FindElementByClassName("ListView");
+            Assert.IsNotNull(mainTheme);
+        }
+    }
+}
