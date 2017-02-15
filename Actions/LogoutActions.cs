1a6a04bbbbdf43cbd70ea9d48b590926c1477323
popcorefucktory <madforcarrie@gmail.com>
2017-02-15 13:02:53 +0300

    Add files via upload

---
+++ Actions/LogoutActions.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectNew.Actions
+{
+    public static class LogoutActions
+
+    {
+        public static void LogoutAction(RemoteWebDriver session)
+
+        {
+            session.FindElementByName("Мой ivi").Click();
+            session.FindElementByName("Профиль").Click();
+            session.FindElementByName("Выйти").Click();
+            session.FindElementByName("Да").Click();
+        }
+    }
+}
---
+++ Actions/NavigationActions.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestProjectNew.Actions
+{
+    public static class NavigationActions
+    {
+        public static object OpenMyIviEntrance { get; internal set; }
+
+        public static void OpenMyIvi(RemoteWebDriver session)
+            {
+            session.FindElementByName("Мой ivi").Click();
+            session.FindElementByName("Войти").Click();
+            }
+
+
+    }
+}
