1f6ca60220b2dfae688b67ef88e35b7914fffbf4
popcorefucktory <madforcarrie@gmail.com>
2017-02-16 19:08:41 +0300

    Add files via upload

---
+++ ContentCard.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+
+namespace TestProjectNew
+{
+    [TestClass]
+    public class ContsntCard
+        private static RemoteWebDriver _session;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _session = SetupActions.LaunchApp();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _session.Dispose();
+            _session = null;
+        }
+
+        [TestMethod]
+        public void ScrollDown()
+        {
+
+        }
+    }
+}
