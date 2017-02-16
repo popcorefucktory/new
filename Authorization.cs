757d929d8bef4a94686f3eb4c8f5610ebf21ab71
popcorefucktory <madforcarrie@gmail.com>
2017-02-16 17:14:28 +0300

    Add files via upload

---
+++ Authorization.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+using OpenQA.Selenium;
+using TestProjectNew.Actions;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProjectNew
+{
+    [TestClass]
+    public class Authorization
+    {
+        private static RemoteWebDriver _session;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _session = SetupActions.LaunchApp();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _session.Dispose();
+            _session = null;
+        }
+
+
+        [TestMethod]
+        public void OpenMyIviEntrance()
+        {
+            // Act
+            NavigationActions.OpenMyIvi(_session);
+
+            // Assert
+
+            Assert.IsNotNull(_session.FindElementByName("Вход"));
+            
+        }
+
+        [TestMethod]
+        public void EnterTheEmailSuccessful()
+        {
+            // Arrange
+            NavigationActions.OpenMyIvi(_session);
+
+            // Act
+            _session.FindElementByClassName("TextBlock").SendKeys("kivyanskaya@ivi.ru");
+            _session.FindElementByClassName("Button").Click();
+            var passpage = _session.FindElementByClassName("PasswordBox");
+
+            Assert.IsNotNull(passpage);
+         
+        }
+
+        [TestMethod]
+        public void AutoNon_ExistentMailVerifyPopup()
+        {
+            // Arrange 
+            NavigationActions.OpenMyIvi(_session);
+
+            // Act
+            _session.FindElementByClassName("TextBlock").SendKeys("111111");
+            _session.FindElementByClassName("Button").Click();
+            string errorMsg = _session.FindElementByName("Вы ввели несуществующий телефон или email.").Text;
+            Console.WriteLine(errorMsg);
+
+            // Assert
+            Assert.IsNotNull(errorMsg);
+        }
+
+        [TestMethod]
+        public void AutoNon_ExistentMailPopupClickOk()
+        {
+            NavigationActions.OpenMyIvi(_session);
+
+            _session.FindElementByClassName("TextBlock").SendKeys("kivyanskaya@ivi");
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByName("OK").Click();
+            string reg = _session.FindElementByName("Регистрация").Text;
+            Console.WriteLine(reg);
+
+            Assert.IsNotNull(reg);
+        }
+
+        [TestMethod]
+        public void AutoNon_ExistentMailClickBack()
+        {
+            NavigationActions.OpenMyIvi(_session);
+
+            _session.FindElementByClassName("TextBlock").SendKeys("111111");
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByClassName("Button").SendKeys(Keys.Escape);
+            var textBox = _session.FindElementByClassName("TextBox");
+
+            Assert.IsNotNull(textBox);
+        }
+
+        [TestMethod]
+        public void ClearType()
+        {
+            NavigationActions.OpenMyIvi(_session);
+
+            _session.FindElementByClassName("TextBlock").SendKeys("111111");
+            _session.FindElementByClassName("TextBox").Clear();
+            Thread.Sleep(3000);
+            //WaitForElementPresent;
+            var c = _session.FindElementByClassName("TextBlock").Text;
+            Console.WriteLine(c);
+
+            //Assert
+            
+            //здесь нужно найти пустоту в поле
+
+            //Assert проверка, что поле пусто 
+
+        }
+        [TestMethod]
+        public void EnterThePasswordSuccsessful()
+        {
+            EnterTheEmailSuccessful();
+            _session.FindElementByClassName("PasswordBox").SendKeys("12345");
+            _session.FindElementByName("Войти").Click();
+            _session.FindElementByName("OK").Click();
+            var focusable = _session.FindElementByClassName("ListView");
+
+            Assert.IsNotNull(focusable);
+        }
+        [TestMethod]
+        public void EnterThePasswordFailClickOk()
+        {
+            EnterTheEmailSuccessful();
+            _session.FindElementByClassName("PasswordBox").SendKeys("1234");
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByName("OK").Click();
+            var textBox = _session.FindElementByClassName("PasswordBox");
+
+            Assert.IsNotNull(textBox);
+            
+
+        }
+        [TestMethod]
+        public void AuthorisationPhoneSuccess()
+        {
+           
+            //LogoutActions.LogoutAction(_session);
+            NavigationActions.OpenMyIvi(_session);
+
+            _session.FindElementByClassName("TextBox").Clear();
+            _session.FindElementByClassName("TextBlock").SendKeys("79998660609");
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByName("OK").Click();
+
+            Phone_MailCatcherActions.OpenMailcatcher(_session);
+            
+        }
+        [TestMethod]
+        public void AuthorisationPhoneFail()
+        {
+            _session.FindElementByClassName("TextBlock").SendKeys("77777");
+
+        }
+
+        [TestMethod]
+        public void AuthorisationPhoneFailVerifyPopup()
+        {
+            NavigationActions.OpenMyIvi(_session);
+
+            _session.FindElementByClassName("TextBlock").SendKeys("7777");
+            _session.FindElementByClassName("Button").Click();
+            string errorMsg2 = _session.FindElementByName("Ошибка").Text;
+            Console.WriteLine(errorMsg2);
+
+            //Assert.IsNotNull();
+            //Assert.IsNotNull(_session.FindElementByClassName("TextBox"));
+        }
+
+        [TestMethod]
+        public void AuthorisationPhoneFailClickBack()
+        {
+            NavigationActions.OpenMyIvi(_session);
+
+            _session.FindElementByClassName("TextBlock").SendKeys("7777777");
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByClassName("Button").SendKeys(Keys.Escape);
+
+            Assert.IsNotNull(_session.FindElementByClassName("TextBox"));
+        }
+
+        
+    }
+}
+
+
---
+++ Registration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Threading;
+using TestProjectNew.Actions;
+
+namespace TestProjectNew
+{
+
+    [TestClass]
+    public class Registration
+    {
+        private static RemoteWebDriver _session;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _session = SetupActions.LaunchApp();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _session.Dispose();
+            _session = null;
+        }
+
+        [TestMethod]
+        public void _registrationByPhone_success()
+            
+        {
+            NavigationActions.OpenMyIvi(_session);
+
+            _session.FindElementByClassName("TextBlock").SendKeys("79999999999");
+            _session.FindElementByName("Регистрация").Click();
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByClassName("Button").SendKeys(Keys.Enter);
+            Phone_MailCatcherActions.OpenMailcatcher(_session);
+            var maintheme = _session.FindElementByClassName("ListView");
+
+            Assert.IsNotNull(maintheme);
+         }
+
+        [TestMethod]
+        public void _registrationByMail_success()
+        {
+            NavigationActions.OpenMyIvi(_session);
+            _session.FindElementByName("Регистрация").Click();
+            ChromeDriver EmailGenerator = new ChromeDriver();
+            EmailGenerator.Url = "http://www.yopmail.com/ru/email-generator.php";
+            EmailGenerator.Navigate();
+            var randomMail = EmailGenerator.FindElementByXPath("//*[@id='login']").GetAttribute("value");
+            _session.FindElementByClassName("TextBlock").SendKeys(randomMail);
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByClassName("TextBlock").SendKeys("1");
+            _session.FindElementByClassName("Button").Click();
+            _session.FindElementByClassName("Button").Click();
+            Thread.Sleep(2000);
+
+            //Console.WriteLine(a);
+
+            // _session.FindElementByClassName("TextBlock").SendKeys(key);
+        }
+
+    }
+}
