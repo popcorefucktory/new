0348e95fbc2ebf61796ddc0b5eb1c826eb816c15
popcorefucktory <madforcarrie@gmail.com>
2017-04-01 22:29:55 +0300

    Add files via upload

---
+++ CertActivate/SimpleCertActivate_Sucsess().cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using TestProjectNew.Actions;
+
+namespace TestProjectNew
+
+{
+    [TestClass]
+    public class SimpleCertActivate
+    {
+        private static RemoteWebDriver _session;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _session = SetupActions.LaunchApp();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _session.Dispose();
+            _session = null;
+        }
+        [TestMethod]
+        public void SimpleCertActivate_Sucsess()
+        {
+
+            var a = GenerateCertActions.GenerateCert(_session);
+            _session.FindElementByName("myivi").Click();
+            _session.FindElementByName("Активировать сертификат").Click();
+            _session.FindElementByClassName("TextBox").SendKeys(a);
+
+        }
+    }
+}
